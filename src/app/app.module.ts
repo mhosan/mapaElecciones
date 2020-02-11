93d6d42fbb33e428d748c270d82e2d71a0328927
Marcelo Hosan <mhosan@gmail.com>
2020-02-11 15:54:23 -0300

    limpiando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,13 @@ import { AppComponent } from './app.component';
 
 import { MapaComponent } from './mapa/mapa.component'
 //import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { UsaApiComponent } from './geoloca/usa-api-secr-modern/usa-api.component';
-import { WebappComponent } from './webapp/webapp.component';
+import { UsaApiComponent } from '../app/componentes/geoloca/usa-api-secr-modern/usa-api.component';
 import { GeolocaApiSecrModernService } from './servicios/geoloca-api-secr-modern.service';
-import { UsaApiLocationIqComponent } from './geoloca/usa-api-location-iq/usa-api-location-iq.component';
+import { UsaApiLocationIqComponent } from '../app/componentes/geoloca/usa-api-location-iq/usa-api-location-iq.component';
 import { EleccionesService } from './servicios/elecciones.service';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from './../environments/environment';
-import { PartidosFormComponent } from './mapa/partidos-form/partidos-form.component';
 import { BnavegaComponent } from './componentes/bnavega/bnavega.component';
 import { UsuarioComponent } from './componentes/usuario/usuario.component';
 
@@ -24,9 +22,7 @@ import { UsuarioComponent } from './componentes/usuario/usuario.component';
     AppComponent,
     MapaComponent,
     UsaApiComponent,
-    WebappComponent,
     UsaApiLocationIqComponent,
-    PartidosFormComponent,
     BnavegaComponent,
     UsuarioComponent
   ],
@@ -47,8 +43,7 @@ import { UsuarioComponent } from './componentes/usuario/usuario.component';
     GeolocaApiSecrModernService,
     EleccionesService
   ],
-  bootstrap: [AppComponent],
-  entryComponents: [PartidosFormComponent]
+  bootstrap: [AppComponent]
 })
 
 export class AppModule { }

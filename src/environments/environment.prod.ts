8b52ee7840a41b9be27e6fbb9cecbd491cad10b1
marcelo <mhosan@gmail.com>
2020-03-16 20:05:33 -0300

    proyecto nuevo (argengis.web.app), pero usa la bd del proyecto viejo (argengis-60763.web.app)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+/*
+Utilizar la primer definicion de la constante "firebase" para usar la 
+base de datos del proyecto viejo (el que tenia como nombre de dominio
+https://argengis-60763.web.app).
+
+La segunda definicion de la constante "firebase" es la que corresponde
+al proyecto nuevo (el que tiene como nombre de dominio 
+https://argengis.web.app)
+
+*/
 export const environment = {
   production: true,
   firebase : {
@@ -10,3 +20,17 @@ export const environment = {
     appId: "1:944972773597:web:edc500e7cc508693"
   }
 };
+
+// export const environment = {
+//   production: true,
+//   firebase: {
+//     apiKey: "AIzaSyCmZJPmgSH_bZ1D8GeM89RqkzgLmLUW-c4",
+//     authDomain: "argengis.firebaseapp.com",
+//     databaseURL: "https://argengis.firebaseio.com",         
+//     projectId: "argengis",                                  
+//     storageBucket: "argengis.appspot.com",                  
+//     messagingSenderId: "389370963295",                      
+//     appId: "1:389370963295:web:dc96461a3d22a7787e02ea",
+//     measurementId: "G-5G6F9WMZZD"
+//   }
+// };

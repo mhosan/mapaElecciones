b68d883a1ae861992e42735b25cd3b2ad280cfbd
marcelo <mhosan@gmail.com>
2020-03-21 21:44:23 -0300

    ajustando la vista

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   firebase: {
-//     apiKey: "AIzaSyBBLjXy4C9SWDNm2XmczSppIh3xL6ZSt9I",
-//     authDomain: "argengis-60763.firebaseapp.com",
-//     databaseURL: "https://argengis-60763.firebaseio.com",
-//     projectId: "argengis-60763",
-//     storageBucket: "argengis-60763.appspot.com",
-//     messagingSenderId: "944972773597",
-//     appId: "1:944972773597:web:edc500e7cc508693"
-//   }
-// };
 export const environment = {
   production: false,
   firebase: {

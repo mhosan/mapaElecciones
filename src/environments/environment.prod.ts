24be5d8b7eb35bbd484715de0d7f0db54f9f3c85
marcelo <mhosan@gmail.com>
2020-03-24 19:54:22 -0300

    nada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,29 +8,29 @@ al proyecto nuevo (el que tiene como nombre de dominio
 https://argengis.web.app)
 
 */
-export const environment = {
-  production: true,
-  firebase : {
-    apiKey: "AIzaSyBBLjXy4C9SWDNm2XmczSppIh3xL6ZSt9I",
-    authDomain: "argengis-60763.firebaseapp.com",
-    databaseURL: "https://argengis-60763.firebaseio.com",
-    projectId: "argengis-60763",
-    storageBucket: "argengis-60763.appspot.com",
-    messagingSenderId: "944972773597",
-    appId: "1:944972773597:web:edc500e7cc508693"
-  }
-};
-
 // export const environment = {
 //   production: true,
-//   firebase: {
-//     apiKey: "AIzaSyCmZJPmgSH_bZ1D8GeM89RqkzgLmLUW-c4",
-//     authDomain: "argengis.firebaseapp.com",
-//     databaseURL: "https://argengis.firebaseio.com",         
-//     projectId: "argengis",                                  
-//     storageBucket: "argengis.appspot.com",                  
-//     messagingSenderId: "389370963295",                      
-//     appId: "1:389370963295:web:dc96461a3d22a7787e02ea",
-//     measurementId: "G-5G6F9WMZZD"
+//   firebase : {
+//     apiKey: "AIzaSyBBLjXy4C9SWDNm2XmczSppIh3xL6ZSt9I",
+//     authDomain: "argengis-60763.firebaseapp.com",
+//     databaseURL: "https://argengis-60763.firebaseio.com",
+//     projectId: "argengis-60763",
+//     storageBucket: "argengis-60763.appspot.com",
+//     messagingSenderId: "944972773597",
+//     appId: "1:944972773597:web:edc500e7cc508693"
 //   }
 // };
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCmZJPmgSH_bZ1D8GeM89RqkzgLmLUW-c4",
+    authDomain: "argengis.firebaseapp.com",
+    databaseURL: "https://argengis.firebaseio.com",         
+    projectId: "argengis",                                  
+    storageBucket: "argengis.appspot.com",                  
+    messagingSenderId: "389370963295",                      
+    appId: "1:389370963295:web:dc96461a3d22a7787e02ea",
+    measurementId: "G-5G6F9WMZZD"
+  }
+};
